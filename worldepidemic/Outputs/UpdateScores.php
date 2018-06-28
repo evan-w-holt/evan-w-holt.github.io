ed725761ef6486f484bc9b94e803f1b18989eb98
Evan Holt <holtevan@grinnell.edu>
2018-06-28 16:37:53 -0500

    PHP bug fix maybe

--- worldepidemic/Outputs/UpdateScores.php
+++ worldepidemic/Outputs/UpdateScores.php
@@ -1,6 +1,6 @@
 <?php
-	if(isset($_POST['txt'])){
-		file_put_contents("Leaderboard.txt", $_POST['txt']);
+	if(isset($_POST['txt']) && isset($_POST['file'])){
+		file_put_contents($_POST['file'].".txt", $_POST['txt']);
 		echo "uploaded.";
 	}else{
 		echo "invalid.";
